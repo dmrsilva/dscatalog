4fd7985a8c7307aba20e0922e29a82a0af4d3077
Daniel Silva <daniel.mascr@gmail.com>
2022-05-29 11:52:38 -0300

    Routes

---
+++ frontweb/src/Routes.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
+import Admin from 'pages/Admin';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/products">
+        <Catalog />
+      </Route>
+      <Route path="/admin">
+        <Admin />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
