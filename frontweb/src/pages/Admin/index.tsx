9ae0a2f6fecb46ca3b686e595b4b19520ef03fa5
Daniel Silva <daniel.mascr@gmail.com>
2022-05-30 17:01:19 -0300

    Admin Navbar layout mobile

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,5 +1,16 @@
+import Navbar from "./Navbar";
+
+import './styles.css';
+
 const Admin = () => {
-  return <h1>Página Admin</h1>;
+  return (
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+          <h1>Conteúdo</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
