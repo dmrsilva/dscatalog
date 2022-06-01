d1d0dd6ef671f8792b57b84b10049e1aef21437e
Daniel Silva <daniel.mascr@gmail.com>
2022-06-01 16:53:12 -0300

    Saving auth data to localStorage

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import qs from 'qs';
 
+type LoginResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+}
+
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+const tokenKey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_BACKEND_URL ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_BACKEND_URL ?? 'dscatalog123';
@@ -30,4 +41,13 @@ export const requestBackendLogin = (loginData: LoginData) => {
     data,
     headers,
   });
-};
+}
+
+export const saveAuthData = (obj : LoginResponse) => {
+  localStorage.setItem(tokenKey, JSON.stringify(obj));
+}
+
+export const getAuthData = () => {
+  const str = localStorage.getItem(tokenKey) ?? "{}";
+  return JSON.parse(str) as LoginResponse;
+}
