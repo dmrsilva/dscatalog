3526ee99e8956569221f152ec32f7184da5f3d6a
Daniel Silva <daniel.mascr@gmail.com>
2022-06-03 15:17:55 -0300

    Product crud form HTML

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,8 +1,33 @@
+import './styles.css';
 
 const Form = () => {
 
     return (
-        <h1>Tela Form</h1>
+        <div className="product-crud-container">
+            <div className="base-card product-crud-form-card">
+            <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+
+            <form action="">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <input type="text" className="form-control base-input" />
+                        <input type="text" className="form-control base-input" />
+                        <input type="text" className="form-control base-input" />
+                    </div>
+                    <div className="col-lg-6">
+                        <textarea rows={10} className="form-control base-input"></textarea>
+                    </div>
+                </div>
+                <div>
+                    <button className="btn btn-outline-danger">CANCELAR</button>
+                    <button className="btn btn-primary">SALVAR</button>
+                </div>
+
+            </form>
+
+            </div>
+
+        </div>
     )
 }
 
