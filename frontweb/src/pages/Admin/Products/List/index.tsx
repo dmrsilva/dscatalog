ce9b4501f457b18d8cd22c2f48ff88fff0a66504
Daniel Silva <daniel.mascr@gmail.com>
2022-06-03 16:36:27 -0300

    Product listing from backend

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,34 +1,31 @@
 import ProductCrudCard from '../ProductCrudCard';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Product } from 'types/product';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
 import './styles.css';
 
-const product = {
-  id: 2,
-  name: 'Smart TV',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  price: 2190.0,
-  imgUrl:
-    'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-  date: '2020-07-14T10:00:00Z',
-  categories: [
-    {
-      id: 1,
-      name: 'Livros',
-    },
-    {
-      id: 2,
-      name: 'Computadores',
-    },
-    {
-      id: 3,
-      name: 'Eletrônicos',
-    },
-  ],
-};
-
 const List = () => {
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
+      },
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
+
   return (
     <div className="product-crud-container">
       <div className="product-crud-bar-container">
@@ -40,15 +37,11 @@ const List = () => {
         <div className="base-card product-filter-container">Search Bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((product) => (
+          <div className="col-sm-6 col-md-12" key={product.id}>
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
     </div>
   );
