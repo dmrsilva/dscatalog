cb0ad070928dfef657266df4d0e48f0f776e4d41
Daniel Silva <daniel.mascr@gmail.com>
2022-06-03 15:39:53 -0300

    Form buttons styling

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,34 +1,42 @@
 import './styles.css';
 
 const Form = () => {
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
-    return (
-        <div className="product-crud-container">
-            <div className="base-card product-crud-form-card">
-            <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
-
-            <form action="">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <input type="text" className="form-control base-input" />
-                        <input type="text" className="form-control base-input" />
-                        <input type="text" className="form-control base-input" />
-                    </div>
-                    <div className="col-lg-6">
-                        <textarea rows={10} className="form-control base-input"></textarea>
-                    </div>
-                </div>
-                <div>
-                    <button className="btn btn-outline-danger">CANCELAR</button>
-                    <button className="btn btn-primary">SALVAR</button>
-                </div>
-
-            </form>
-
+        <form action="">
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
-
-        </div>
-    )
-}
+            <div className="col-lg-6">
+              <div>
+                <textarea rows={8} className="form-control base-input h-auto" />
+              </div>
+            </div>
+          </div>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
