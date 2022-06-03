e351c9c9f522006bb5461c5765800937fab49d89
Daniel Silva <daniel.mascr@gmail.com>
2022-06-03 10:23:51 -0300

    Product crud routes

---
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,9 @@
+
+const Form = () => {
+
+    return (
+        <h1>Tela Form</h1>
+    )
+}
+
+export default Form;
--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,7 +1,8 @@
-import { Switch} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Navbar from './Navbar';
-import Users from './User';
+import Users from './Users';
 import PrivateRoute from 'components/PrivateRoute';
+import Products from './Products';
 
 import './styles.css';
 
@@ -12,7 +13,7 @@ const Admin = () => {
       <div className="admin-content">
         <Switch>
           <PrivateRoute path="/admin/products">
-            <h1>Product CRUD</h1>
+            <Products />
           </PrivateRoute>
           <PrivateRoute path="/admin/categories">
             <h1>Category CRUD</h1>
