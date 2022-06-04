603b6adbc8b77940ee46ff1fbbc76ad05e7c6738
Daniel Silva <daniel.mascr@gmail.com>
2022-06-04 09:30:45 -0300

    React select layout

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from 'axios';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
+import Select from 'react-select';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
 import './styles.css';
@@ -11,6 +12,13 @@ type UrlParams = {
 };
 
 const Form = () => {
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
   const { productId } = useParams<UrlParams>();
 
   const isEditing = productId !== 'create';
@@ -87,6 +95,15 @@ const Form = () => {
                   {errors.name?.message}
                 </div>
               </div>
+
+
+              <div className="margin-bottom-30">
+                <Select
+                  options={options}
+                  classNamePrefix="product-crud-select"
+                />
+              </div>
+
 
               <div className="margin-bottom-30">
                 <input
