d88ea09e2f4e0a8b1cd031850daa4abd59b085c8
Daniel Silva <daniel.mascr@gmail.com>
2022-06-04 22:06:53 -0300

    Pagination finish

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -5,24 +5,24 @@ import { SpringPage } from 'types/vendor/spring';
 import { Product } from 'types/product';
 import { AxiosRequestConfig } from 'axios';
 import { requestBackend } from 'util/requests';
+import Pagination from 'components/Pagination';
 
 import './styles.css';
-import Pagination from 'components/Pagination';
 
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
   useEffect(() => {
-    getProducts();
+    getProducts(0);
   }, []);
 
-  const getProducts = () => {
+  const getProducts = (pageNumber: number) => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: 0,
-        size: 50,
+        page: pageNumber,
+        size: 3,
       },
     };
 
@@ -44,11 +44,11 @@ const List = () => {
       <div className="row">
         {page?.content.map((product) => (
           <div className="col-sm-6 col-md-12" key={product.id}>
-            <ProductCrudCard product={product} onDelete={() => getProducts()} />
+            <ProductCrudCard product={product} onDelete={() => getProducts(page.number)} />
           </div>
         ))}
       </div>
-      <Pagination />
+      <Pagination pageCount={page ? page.totalPages : 0} range={3} onChange={getProducts}/>
     </div>
   );
 };
--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -14,12 +14,12 @@ const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
+  const getProducts = (pageNumber: number) => {
     const params: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: 0,
+        page: pageNumber,
         size: 12,
       },
     };
@@ -32,6 +32,10 @@ const Catalog = () => {
       .finally(() => {
         setIsLoading(false);
       });
+  };
+
+  useEffect(() => {
+    getProducts(0);
   }, []);
 
   return (
@@ -53,9 +57,12 @@ const Catalog = () => {
           ))
         )}
       </div>
-
       <div className="row">
-        <Pagination />
+        <Pagination
+          pageCount={page ? page.totalPages : 0}
+          range={3}
+          onChange={getProducts}
+        />
       </div>
     </div>
   );
